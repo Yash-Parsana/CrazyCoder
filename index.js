4a430a22f40b63120fbbd44e403c38331c813c2d
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2023-02-08 00:03:07 +0530

    removing congfig.env

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express')
-const axios = require('axios')
-const cheerio = require('cheerio')
-const dotenv = require('dotenv')
 
-dotenv.config({path:'./config.env'})
+
 const app = express()
 
 const scheduleRouter = require('./router/scheduleRoute')
@@ -12,7 +9,7 @@ const ratingRouter=require('./router/ratingRouter')
 app.use('/schedule', scheduleRouter)
 app.use('/ranking',ratingRouter)
 
-const PORT=process.env.PORT
+const PORT=3000
 app.listen(PORT, () => {
     console.log("Server is Runnig on Port : ",PORT);
 })
