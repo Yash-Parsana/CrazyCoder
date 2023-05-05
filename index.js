3086d55d735f8754e6e155c8df8b96ddfb2a4903
Yash-Parsana <77839976+Yash-Parsana@users.noreply.github.com>
2023-05-05 18:51:28 +0530

    Nothing is changed

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express = require('express')
+const express = require('express');
 
+const app = express();
 
-const app = express()
+const scheduleRouter = require('./router/scheduleRoute');
+const ratingRouter = require('./router/ratingRouter');
 
-const scheduleRouter = require('./router/scheduleRoute')
-const ratingRouter=require('./router/ratingRouter')
+app.use('/schedule', scheduleRouter);
+app.use('/ranking', ratingRouter);
 
-app.use('/schedule', scheduleRouter)
-app.use('/ranking',ratingRouter)
-
-app.get('/', () => {
-    console.log("You are in default Root");
-})
+app.get('/', (req,res) => {
+    res.send("You are in default Rought")
+});
 app.listen(3000, () => {
     console.log("Server is Runnig on Port : ",3000);
 })
